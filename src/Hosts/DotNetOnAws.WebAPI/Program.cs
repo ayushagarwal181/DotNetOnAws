51173a22931c6b12a879d9d3ae4e1a11ede9216c
Ayush Agarwal <Ayush.Agarwal@varian.com>
2024-02-10 23:04:22 +0530

    VS solution structured added.

---
+++ src/Hosts/DotNetOnAws.WebAPI/Program.cs
@@ -0,0 +1,30 @@
+// ------------------------------------------------------------------
+// <copyright file="Program.cs" company="Ayush Agarwal.">
+// Copyright © 2024 by Ayush Agarwal. Licensed under the MIT License.
+// </copyright>
+// ------------------------------------------------------------------
+
+namespace DotNetOnAws.WebAPI
+{
+    using Microsoft.AspNetCore.Builder;
+
+    /// <summary>
+    /// The main class for the DotNetOnAws.WebAPI application.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point for the DotNetOnAws.WebAPI application.
+        /// </summary>
+        /// <param name="args">The command-line arguments passed to the application.</param>
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
